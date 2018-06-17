21fec27051fe7e2777a03b24e88009c2fa52144a
begimov <begimov@gmail.com>
2018-06-17 10:17:32 +0300

    StoreUserRequest pulled in RegisterController

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers\Api\Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Http\Requests\Api\Auth\StoreUserRequest;
+
 class RegisterController extends Controller
 {
-    public function register(Request $request)
+    public function register(StoreUserRequest $request)
     {
         return 'REGISTER';
     }
