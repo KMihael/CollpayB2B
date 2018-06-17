44568a3c68453254c1c06f5528c50f8c8a4ec463
begimov <begimov@gmail.com>
2018-06-17 10:31:45 +0300

    User creation has been implemented

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers\Api\Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Models\Users\User;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\Api\Auth\StoreUserRequest;
 
 class RegisterController extends Controller
 {
     public function register(StoreUserRequest $request)
     {
-        return 'REGISTER';
+        User::create([
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
     }
 }
