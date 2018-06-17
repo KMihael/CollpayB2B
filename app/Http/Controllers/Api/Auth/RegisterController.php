b033ccbf258c1b42c6639b846cacfd9f914be0e9
begimov <begimov@gmail.com>
2018-06-17 13:19:23 +0300

    UserTransformer has been created and used to return newly created user from RegisterController's register method

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -7,15 +7,21 @@ use App\Http\Controllers\Controller;
 
 use App\Models\Users\User;
 use Illuminate\Support\Facades\Hash;
+use App\Transformers\Users\UserTransformer;
 use App\Http\Requests\Api\Auth\StoreUserRequest;
 
 class RegisterController extends Controller
 {
     public function register(StoreUserRequest $request)
     {
-        User::create([
+        $user = User::create([
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        return fractal()
+            ->item($user)
+            ->transformWith(new UserTransformer)
+            ->toArray();
     }
 }
