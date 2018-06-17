a1901d3de1f9e4511d7d53d76fd773cb7bb584dc
begimov <begimov@gmail.com>
2018-06-17 14:51:29 +0300

    ProductRepository pulled in ProductController

--- app/Http/Controllers/Api/Products/ProductController.php
+++ app/Http/Controllers/Api/Products/ProductController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers\Api\Products;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Repositories\Contracts\Products\ProductRepository;
 
 class ProductController extends Controller
 {
-    //
+    protected $products;
+
+    public function __construct(ProductRepository $products)
+    {
+        $this->products = $products;
+    }
 }
