fc3098231dc547dc4395e626f34930eeff25e063
begimov <begimov@gmail.com>
2018-06-17 14:59:06 +0300

    StoreProductRequest rules has been implemented

--- app/Http/Controllers/Api/Products/ProductController.php
+++ app/Http/Controllers/Api/Products/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Products;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Repositories\Contracts\Products\ProductRepository;
+use App\Http\Requests\Api\Products\StoreProductRequest;
 
 class ProductController extends Controller
 {
@@ -13,5 +14,10 @@ class ProductController extends Controller
     public function __construct(ProductRepository $products)
     {
         $this->products = $products;
+    }
+
+    public function store(StoreProductRequest $request)
+    {
+        # code...
     }
 }
