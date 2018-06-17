779fdc679f38a3870f83ceb0f328255c9ae757a9
begimov <begimov@gmail.com>
2018-06-17 14:42:14 +0300

    EloquentRepositoryAbstract class has been created

---
+++ app/Repositories/Eloquent/EloquentRepositoryAbstract.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+// use App\Repositories\Exceptions\NoEntityDefined;
+use App\Repositories\RepositoryInterface;
+// use App\Repositories\Criteria\CriteriaInterface;
+
+abstract class EloquentRepositoryAbstract implements RepositoryInterface//, CriteriaInterface
+{
+    protected $entity;
+
+    public function __construct()
+    {
+        $this->entity = $this->resolveEntity();
+    }
+
+    protected function resolveEntity()
+    {
+        if (!method_exists($this, 'entity')) {
+            // throw new NoEntityDefined();
+        }
+        return app()->make($this->entity());
+    }
+}
